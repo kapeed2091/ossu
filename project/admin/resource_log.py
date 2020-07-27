f3c4d8bb90cfd1cc9ade55003c3fc5820891dd08
Deepak Kumar Malladi <deepak@enlume.com>
2020-07-27 23:45:47 +0530

    Add filter for ResourceLog admin panel

--- project/admin/resource_log.py
+++ project/admin/resource_log.py
@@ -5,6 +5,7 @@ from project.models import ResourceLog
 @admin.register(ResourceLog)
 class ResourceLogAdmin(admin.ModelAdmin):
     list_display = ['id', 'project_name', 'resource_name', 'date', 'time', 'description']
+    list_filter = ['project__name', 'resource__username']
 
     def project_name(self, obj):
         return obj.project.name
